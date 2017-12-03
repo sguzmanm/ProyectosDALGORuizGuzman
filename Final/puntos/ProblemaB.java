1ce17ce6e9f11e8b5d24c1309e9c3a8344d2307f
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2017-12-03 11:57:19 -0500

    Ya empecé con documentación, el algoritmo sirve

--- Final/puntos/ProblemaB.java
+++ Final/puntos/ProblemaB.java
@@ -16,50 +16,55 @@ import java.util.Queue;
  */
 public class ProblemaB {
 
-	private String toString(boolean[]b)
-	{
-		String msj="";
-		for(int i=0;i<b.length;i++)
-		{
-			if(b[i]) msj+="1 ";
-			else msj+="0 ";
-		}
-		return msj;
-	}
+	/**
+	 *Retorna la diferencia mínima de haber sumado los grafos BC dados en el arreglo.<br>
+	 * @param graphs Arreglo con grafos que solo poseen la diferencia como atributo.<br>
+	 * @return Diferencia mínima de la suma de los grafos BC. En el caso de que el algoritmo no resuleva el problema, retorna -1.
+	 */
 	private int minimumDifference(Graph[] graphs)
 	{
-		//Variables of max difference and temporal difference
+		//Variables de máxima diferencia y de diferencia temporal
 		int maxDifference=0;
 		int dif=0;
-		//Hash for the graph differences
+		//HashMap para las diferencias de los grafos. Por lo que se entiende, la complejidad de insertar y sacar un elemento es de O(log (n/m)) 
+		//Donde n es el número total de elementos y m la cantidad de biparticiones que se hacen dentro del hashmap. Dicho n siempre será igual al número
+		//N de grafos for lo que la complejidad de inserción se despreciará en este algoritmo al depender de m, es decir, se asume como constante.
 		HashMap<Integer,List<Integer>> hash= new HashMap<>();
-		//Cycle to determine max
-		//P1: Until i the variable in maxDifference is the biggest
-		//t:n-i
+		//Ciclo para determinar la máxima diferencia O(N)
+		//P1: 0<=i<N ^ maxDifference=(max k|0<=k<i|:graphs[k].difference)
+		//t:N-i
 		for(int i=0;i<graphs.length;i++)
 		{
+			//Diferencia temporal
 			dif=graphs[i].difference;
 			if(dif>maxDifference)
 			{
 				maxDifference=dif;
 			}
+			//Si no eciste una lista de esta diferencia creéla
 			if(!hash.containsKey(dif))
 			{
 				hash.put(dif, new ArrayList<>());
 			}
+			//Añada a la diferencia un nuevo vértice i
 			hash.get(dif).add(i);
 		}
-		//Variable for boolean array
+		//R1: maxDifference=(max k|o<=k<N|:graphs[k])
+		//Variable para el arreglo de booleanos
 		boolean[] b=null;
-		//Max difference holds the biggest difference in numbers
+		//Se crea la estructura de la ecuación de recurrencia m usando el número toal de grafos N (graphs.length), la diferencia máxima D
+		//(maxDifference+1, para tener en cuemta una diferencia de cero) y una vez más el número total N para registrar en un arreglo de
+		//booleanos que grafos se están usando y cuáles no.
 		boolean[][][] matrix = new boolean[graphs.length][maxDifference+1][graphs.length];
-		//Definition of unique function for row 1
-		//Iterates only over the differences
+		//Itera únicamente sobre los elementos con diferencias registradas. En el peor de los casos se pasa nuevamente por el número total 
+		//de grafos se asume complejidad O(N).
+		//P2:Asumiendo que se un conjunto se puede representar como un arreglo y que los valores i son dados en forma secuencial. Se tiene la siguiente invariante:
+		//0<=i<hash.keySet().size() ^ (ForAll j|0<=j<i: (Exists c in hash.get(j)|0<=c<N: unico(matrix[0][i],c))
 		for(Integer i:hash.keySet())
 		{
 			matrix[0][i][hash.get(i).get(0)]=true;
 		}
-		//Iterates over the other rows
+		//Itera sobre todas las filas
 		boolean exists=true;
 			for(int i=1;i<matrix.length;i++)
 			{
@@ -132,36 +137,41 @@ public class ProblemaB {
 			
 			
 		}
+		//P: 0<=j<matrix[0].length ^ (ForAll k|0<=k<j: !notZero(matrix[matrix.length-1][k])
+		//En otras palabras, para todas las diferencias antes de la que se está analizando, el arreglo de booleanos debe ser el
+		//vector cero. Si no se retorna el valor de la diferencia buscada.
+		//t:matrix[0].length-j
 		for(int j=0;j<matrix[0].length;j++)
 		{
 			if(notZero(matrix[matrix.length-1][j]))
 			{
+				//j sería la mínima diferencia alcanzable
 				return j;
 			}
 		}
-		//If nothing can be achieved
+		//En el caso improbable de que el algoritmo no encuentre alguna diferencia alcanzable con los datos dados retorna -1
 		return -1;
 	}
 	/**
-	 * 2
-		5 4 0 1 0 2 0 3 2
-		4
-		4 3 0 1 1 2 2 3
-	 * @param bs
-	 * @return
+	 * Verifica que el arreglo dado no es el vector cero.<br>
+	 * @param bs Arreglo de booleanos.<br>
+	 * @return Si el arreglo es el vector cero o no.
 	 */
-	
 	private boolean notZero(boolean[] bs) {
+		//P: 0<=i<bs.length ^ (ForAll j|0<=j<i: !bs[j])
+		//t:bs.length-i
 		for(int i=0;i<bs.length;i++)
 		{
 			if(bs[i]) return true;
 		}
+		//R:0<=i<bs.length ^(ForAll j|0<=j<bs.length: !bs[j])
 		return false;
 	}
 
 	public static void main(String[] args) throws Exception{
-		ProblemaB instancia = new ProblemaB();
-		List<Integer> respuestas=new ArrayList<>();
+		//Instancia del problema
+		ProblemaB instance = new ProblemaB();
+		List<Integer> answers=new ArrayList<>();
 		try ( 
 			InputStreamReader is= new InputStreamReader(System.in);
 			BufferedReader br = new BufferedReader(is);
@@ -184,14 +194,13 @@ public class ProblemaB {
 					if(i==n)
 					{
 						n=null;
-						respuestas.add(instancia.minimumDifference(graphs));
+						answers.add(instance.minimumDifference(graphs));
 					}
 				}
 				line=br.readLine();
-				
-				
 			}
-			for(Integer r:respuestas)
+			//If no one is typing anymore end the algorithm and show the answers
+			for(Integer r:answers)
 				System.out.println(r);
 			
 		}
@@ -200,61 +209,62 @@ public class ProblemaB {
 }
 
 /**
- * Graph class
+ * Clase del grafo
  */
 class Graph
 {
-	//Array for the boolean bipartition
+	//Diferencia de la bipartición
 	public int difference;
 	/**
-	 * Graph constructor that uses a given line with the structure (v e x1 y1 x1 y2... xk yk) to create a graph bipartition.<br>
-	 * @param line Given line with the forma described above
+	 * Constructor del grafo que usa una línea con la estructura (v e x1 y1 x1 y2... xk yk) para crear una bipartición del mismo.<br>
+	 * @param line Línea con la estructura descrita arriba
 	 */
 	public Graph(String line)
 	{
 		int[] bipartition;
-		//Q: The graph is bipartite
-		//Data split O(n)
+		//Q: El grafo es bipartito.
+		//Separar los datos O(n)
 		String[] data=line.split(" ");
-		//Number of vertexes
+		//Número de vértices
 		int v=Integer.parseInt(data[0]);
-		//Adjacency matrix
+		//Matriz de adyacencia
 		boolean[][] adj= new boolean[v][v];
+		//Número de aristas
+		int e=Integer.parseInt(data[1]);
 		//P1:2<=i<data.length ^ 0<=e<Integer.parseInt(data[1]) ^ AdjPairs(data,i,adj)
 		//t1:data.length-e
-		for(int i=2,e=0;i<data.length && e<Integer.parseInt(data[1]);i+=2)
+		for(int i=2,j=0;i<data.length && j<e;i+=2,j++)
 		{
 			adj[Integer.parseInt(data[i])][Integer.parseInt(data[i+1])]=true;
 			adj[Integer.parseInt(data[i+1])][Integer.parseInt(data[i])]=true;
 		}
 		//R1: AdjPairs(data,data.length,adj)
-		//Marked array for each pair until data.length there is an edge in adj
+		//Arreglo de marcados para hacer el BFS
 		boolean[] marked=new boolean[v];
 		bipartition= new int[2];
-		//Regular queue with O(1) operations
+		//Cola regular con operaciones en tiempo O(1)
 		Queue<Integer> agenda=new LinkedList<>();
+		//Por defecto la fuente es el vértice 0. Al ser conexo debe poder llegar a todos los otros.
 		int source=0;
-		if(data.length>2)
-			source=Integer.parseInt(data[2]);
-		//Array that stores the value of the vertixes partition, which is either 0 or 1.
+		//Arreglo que guarda el valor de la bipartición asiganda de cada vértice
 		int[] value=new int[v];
-		//Adds one element to the first number of the bipartition
-		System.out.println(line);
+		//Añade la fuente a la primera bipartición
 		bipartition[0]=1;
 		marked[source]=true;
 		agenda.add(source);
 		int color=0;
 		value[source]=0;
-		//Cycle with complexity O(V)
+		//Peor caso del ciclo total: O(V^2)
+		//Al ser un grafo conectado, la agenda tendra como máximo todos los vértices del grafo (V) 
 		//P2:!agenda.isEmpty() ^ bip(marked,value,0)=bipartition[0]^bip(marked,value,1)=bipartition[1]
 		//t2:agenda.size()
 		while(!agenda.isEmpty())
 		{
-			//New source of bfs
+			//Nueva fuente del BFS
 			source=agenda.poll();
-			//Changes the value of the given vertex to the other possible bipartition for the children found in the bfs
+			//Cambia el color del vértice a la siguiente bipartición para asignársela a sus hijos
 			color=(value[source]+1)%2;
-			//Cycle that goes through all the adjoint vertexes. O(V)
+			//Ciclo que va por una fila completa de la matriz de adjuntos (O(V))
 			//P3:0<=i<v ^ markedValue(adj,source,i,marked,value)
 			//t3:v-i
 			for(int i=0;i<v;i++)
@@ -270,7 +280,7 @@ class Graph
 			//R3:markedValue(adj,source,v,marked,value)
 		}
 		//R2: agenda.isEmpty() ^ bip(marked,value,0)=bipartition[0]^bip(marked,value,1)=bipartition[1] ^ generalMarked(adj,v,marked,value)
+		//Se guarda la diferencia de las dos biparticiones
 		difference=Math.abs(bipartition[1]-bipartition[0]);
-		System.out.println("DIFFERENCE "+difference);
 	}
 }
